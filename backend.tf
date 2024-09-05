20305a533cce63661815634b65042e04c76299e1
Shridhar Rodge <56276945+shridhar1312@users.noreply.github.com>
2024-09-05 15:57:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,22 +1,23 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region = "us-east-1"
+}
 
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "shridhar131296"
-#   #acl    = "private"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "shridhar131296"
+  acl    = "private"
 
-#   tags = {
-#     Name = "terraform-state"
-#   }
-# }
+  tags = {
+    Name = "terraform-state"
+  }
+}
 
-# resource "aws_dynamodb_table" "terraform_lock" {
-#   name         = "shridhar131296"
-#   hash_key     = "LockID"
-#   billing_mode = "PAY_PER_REQUEST"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "shridhar131296"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
