5f94dfacb68f4a4301904a7b342166213d301c7d
Shridhar Rodge <56276945+shridhar1312@users.noreply.github.com>
2024-09-05 17:03:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,23 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "shridhar131296"
-  acl    = "private"
-
-  tags = {
-    Name = "terraform-state"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "shridhar131296"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-  
-  attribute {
-    name = "LockID"
-    type = "S"
+terraform {
+  backend "s3" {
+    bucket         = "shridhar131296"          # Replace with your S3 bucket name
+    key            = "terraform.tfstate" # Path to the state file within the bucket
+    region         = "us-east-1"                    # AWS region where the S3 bucket is located
+    encrypt        = true                           # Encrypt the state file
+    dynamodb_table = "your-dynamodb-table-name"     # Replace with your DynamoDB table for state locking
   }
 }
