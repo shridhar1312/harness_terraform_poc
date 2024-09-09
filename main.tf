f0dff5d03efcdcb7199d5c61af08f5d229704519
Shridhar Rodge <56276945+shridhar1312@users.noreply.github.com>
2024-09-09 13:05:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,22 @@ resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allowing SSH from anywhere (you can restrict it)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
