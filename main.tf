6b3638330730ac84c59c5d678284443308d68db6
Shridhar Rodge <56276945+shridhar1312@users.noreply.github.com>
2024-09-09 20:50:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,3 +57,22 @@ resource "aws_security_group" "allow_http" {
   }
 }
 
+resource "aws_security_group" "allow_https" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allowing SSH from anywhere (you can restrict it)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
