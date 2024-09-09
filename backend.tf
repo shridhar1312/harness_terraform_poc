7bd360d97b83f98ef8108d9628e24383f4f97aa6
Shridhar Rodge <56276945+shridhar1312@users.noreply.github.com>
2024-09-09 12:35:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "shridhar131"          # Replace with your S3 bucket name
+    bucket         = "shridhar13"          # Replace with your S3 bucket name
     key            = "terraform.tfstate" # Path to the state file within the bucket
     region         = "us-east-1"                    # AWS region where the S3 bucket is located
     encrypt        = true                           # Encrypt the state file
-    dynamodb_table = "shridhar131"     # Replace with your DynamoDB table for state locking
+    dynamodb_table = "shridhar13"     # Replace with your DynamoDB table for state locking
   }
 }
